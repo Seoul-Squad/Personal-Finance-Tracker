7e405c2ee86d41513f99b65bd0058c2b781a28a3
edrees <muhammed.edrees101@gmail.com>
2025-04-09 12:04:53 +0200

    Removed unnecessary comments

--- src/model/TransactionType.kt
+++ src/model/TransactionType.kt
@@ -1,12 +1,6 @@
 package model
 
 
-/**
- * Represents the type of financial transaction.
- *
- * - [INCOME]: Money received.
- * - [EXPENSE]: Money spent.
- */
 enum class TransactionType {
     INCOME,
     EXPENSE
