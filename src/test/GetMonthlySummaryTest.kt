136edc29f7588390dce1a80191c7611ea40493f0
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-04-10 13:28:44 +0200

    edit GetMonthlySummaryTest logic

--- src/test/GetMonthlySummaryTest.kt
+++ src/test/GetMonthlySummaryTest.kt
@@ -5,65 +5,80 @@ import TransactionManager
 import model.Transaction
 import model.TransactionType
 import storage.InMemoryTransactionStorage
+import storage.TransactionStorage
 import java.time.LocalDate
 
 fun main() {
-    val result = TransactionManager(InMemoryTransactionStorage()).getMonthlySummary()
-    val transactions = listOf(
-        Transaction(
-            id = "1",
-            amount = 265.5,
-            type = TransactionType.EXPENSE,
-            category = "Food",
-            date = LocalDate.parse("2025-12-10"),
-        ),
-        Transaction(
-            id = "2",
-            amount = 265.5,
-            type = TransactionType.EXPENSE,
-            category = "Food",
-            date = LocalDate.parse("2025-07-12"),
-        ),
-        Transaction(
-            id = "3",
-            amount = 265.5,
-            type = TransactionType.EXPENSE,
-            category = "Food",
-            date = LocalDate.parse("2025-04-10"),
-        ),
-        Transaction(
-            id = "4",
-            amount = 2654.5,
-            type = TransactionType.INCOME,
-            category = "Food",
-            date = LocalDate.parse("2025-08-12"),
-        ),
-        Transaction(
-            id = "5",
-            amount = 265.5,
-            type = TransactionType.EXPENSE,
-            category = "Food",
-            date = LocalDate.parse("2025-11-22"),
-        ),
-    )
-    check(
-        name = "return true when result are not empty",
-        result = transactions.isNotEmpty(),
-        correctResult = true
-    )
+    val dummyStorage = object : TransactionStorage {
+        private val transactions = listOf(
+            Transaction(
+                id = "1",
+                amount = 265.5,
+                type = TransactionType.EXPENSE,
+                category = "Food",
+                date = LocalDate.parse("2025-12-10"),
+            ),
+            Transaction(
+                id = "2",
+                amount = 265.5,
+                type = TransactionType.EXPENSE,
+                category = "Food",
+                date = LocalDate.parse("2025-07-13"),
+            ),
+            Transaction(
+                id = "3",
+                amount = 265.5,
+                type = TransactionType.EXPENSE,
+                category = "Food",
+                date = LocalDate.parse("2025-04-10"),
+            ),
+            Transaction(
+                id = "4",
+                amount = 2654.5,
+                type = TransactionType.INCOME,
+                category = "Food",
+                date = LocalDate.parse("2025-07-12"),
+            ),
+            Transaction(
+                id = "5",
+                amount = 265.5,
+                type = TransactionType.EXPENSE,
+                category = "Food",
+                date = LocalDate.parse("2025-12-22"),
+            ),
+        )
 
-    check(
-        name = "return true when result are empty",
-        result = result.isEmpty(),
-        correctResult = true
+        override fun save(transaction: Transaction) {
+        }
+
+        override fun edit(transaction: Transaction) {
+        }
+
+        override fun delete(transactionId: String) {
+        }
+
+        override fun load(): List<Transaction> {
+            return transactions
+        }
+    }
+
+    val transactionManager = TransactionManager(dummyStorage)
+
+
+    println(transactionManager.getMonthlySummary())
+
+    checkEquals(
+        name = "When given a list of transactions that's around 3 months, the summaries list should be of size 3",
+        result = transactionManager.getMonthlySummary().size,
+        correctResult = 3,
     )
 }
 
-fun check(name: String, result: Boolean, correctResult: Boolean) {
+fun checkEquals(name: String, result: Int, correctResult: Int) {
     if (result == correctResult) {
         println("Passed - $name")
     } else {
-        println("Failed - $name")
+        println("\u001B[31mFailed - $name\u001B[0m")
     }
 }
 
