8acd5ad150bd683a9839dd85b629d723e5160dbc
Sara Ashraf <saraashraf1822004@gmail.com>
2025-04-10 16:56:44 +0200

    remove unnecessary function

--- src/Main.kt
+++ src/Main.kt
@@ -21,7 +21,3 @@ fun isValidTransactionDate(date: String): Boolean {
 
 }
 
-fun isValidUUID(uuid: String): Boolean {
-    val uuidRegex = Regex("^[{]?[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}[}]?$")
-    return uuidRegex.matches(uuid)
-}
