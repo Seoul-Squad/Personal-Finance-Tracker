715919a7d0232a1fc054ca0ff81c224cea124620
Sara Ashraf <saraashraf1822004@gmail.com>
2025-04-10 17:44:51 +0200

    add isTransactionValid function and test cases for this function

---
+++ src/TransactionValidator.kt
@@ -0,0 +1,9 @@
+import model.Transaction
+
+object TransactionsValidator {
+    fun isTransactionValid(transaction: Transaction): Boolean {
+        val isAmountValid = transaction.amount != null && transaction.amount >= 0
+        val isCategoryValid = transaction.category.isNotBlank()
+        return isAmountValid && isCategoryValid
+    }
+}
