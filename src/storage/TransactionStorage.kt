002151f7e64f2138fc8b3f6ac621d4783d390f60
edrees <muhammed.edrees101@gmail.com>
2025-04-10 10:29:26 +0200

    Implement TransactionStorage delete method

--- src/storage/TransactionStorage.kt
+++ src/storage/TransactionStorage.kt
@@ -6,6 +6,6 @@ import model.Transaction
 interface TransactionStorage {
     fun save(transaction: Transaction)
     fun edit(transaction: Transaction)
-    fun delete(transactionId: String)
+    fun delete(transactionId: String): Boolean
     fun load(): List<Transaction>
 }
