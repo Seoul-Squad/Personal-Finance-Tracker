6fb6f5c92da7a25be2990c44593db7cd276ca3a3
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-04-10 12:14:11 +0200

    add getAllTransaction method add printAllTransactions method

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import model.Transaction
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+}
+
+fun getAllTransactions(transactionManager: TransactionManager) {
+    val allTransactions = transactionManager.getAll()
+
+    printAllTransactions(allTransactions)
+}
+
+fun printAllTransactions(transactions: List<Transaction>) {
+    println("\u001b[1m\u001b[4mAll transactions:\u001B[0m \n") // Bold() + Underlined()
+
+    println("\u001B[1m+------+------------------+---------------------+-----------+-------------------------+")
+    println("| ID   | Amount           | Category            | Type      |   Date(dd/mm/yyyy)      |")
+    println("+------+------------------+---------------------+-----------+-------------------------+")
+
+    transactions.forEach { transaction ->
+        val dateString = "${transaction.date.dayOfMonth}/${transaction.date.monthValue}/${transaction.date.year}"
+        val amount = "${transaction.amount} $"
+
+        println(
+            "| ${transaction.id.padEnd(4)} | ${amount.padEnd(16)} | ${transaction.category.padEnd(19)} | ${
+                transaction.type.toString().padEnd(9)
+            } |  ${dateString.padEnd(22)} |"
+        )
     }
+
+    println("+------+------------------+---------------------+-----------+-------------------------+\u001b[0m")
 }
