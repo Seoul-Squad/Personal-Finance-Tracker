5a5120f355d70c1aa66879ce9d8b617ca8f7acc4
Khaled.Eid <Khaled.Eid@souhoola.com>
2025-04-11 00:27:05 +0200

    add maven to project

--- src/main/java/model/Transaction.kt
+++ src/main/java/model/Transaction.kt
@@ -1,15 +1,19 @@
 package model
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import storage.util.LocalDateSerializer
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
+@Serializable
 data class Transaction(
     val id: String,
     val amount: Double,
     val type: TransactionType,
     val category: String,
-    val date: LocalDate,
-) {
+    @Serializable(with = LocalDateSerializer::class)
+    val date: LocalDate = LocalDate.now()) {
     fun validateTransaction (): List<String> {
         val errors = mutableListOf<String>()
         if (amount <= 0)
