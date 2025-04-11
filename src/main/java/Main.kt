de69f9eacbd781f0895692f3048e710eca98669b
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-04-11 19:49:46 +0200

    remove unused imports

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -2,14 +2,7 @@ package main.java
 
 import InFileTransactionStorage
 import TransactionManager
-import model.Transaction
-import model.TransactionType
-import main.java.utils.AnsiColor
-import java.util.UUID
-import java.time.LocalDate
-import model.MonthlySummary
 import storage.TransactionStorage
-import java.time.format.DateTimeFormatter
 
 fun main() {
     val storage: TransactionStorage = InFileTransactionStorage
