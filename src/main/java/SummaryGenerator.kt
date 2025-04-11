019dde2881fdd2ae24f57265d962c79e474bbe13
Mohamed Hesham <medo93346@gmail.com>
2025-04-11 22:03:46 +0200

    add summaryGenerator class

---
+++ src/main/java/SummaryGenerator.kt
@@ -0,0 +1,24 @@
+package main.java
+
+import model.MonthlySummary
+import model.Transaction
+import model.TransactionType
+import java.time.format.DateTimeFormatter
+
+class SummaryGenerator {
+    fun generateMonthlySummaries(transactions: List<Transaction>): List<MonthlySummary> {
+        val grouped = transactions.groupBy {
+            it.date.format(DateTimeFormatter.ofPattern("MM-yyyy"))
+        }
+        val summaries = grouped.map { (monthYear, transactions) ->
+            val totalIncome = transactions
+                .filter { it.type == TransactionType.INCOME }
+                .sumOf { it.amount ?: 0.0 }
+            val totalExpense = transactions
+                .filter { it.type == TransactionType.EXPENSE }
+                .sumOf { it.amount ?: 0.0 }
+            MonthlySummary(monthYear, totalIncome, totalExpense)
+        }
+        return summaries.sortedBy { it.monthYear }
+    }
+}
--- src/main/java/TransactionManager.kt
+++ src/main/java/TransactionManager.kt
@@ -1,11 +1,11 @@
+import main.java.SummaryGenerator
 import model.MonthlySummary
 import model.Transaction
-import model.TransactionType
 import storage.TransactionStorage
-import java.time.format.DateTimeFormatter
 
 class TransactionManager(
-    private val storage: TransactionStorage
+    private val storage: TransactionStorage,
+    private val summaryGenerator: SummaryGenerator
 ) {
     fun addTransaction(transaction: Transaction): List<String> {
         val errors = transaction.validateTransaction()
@@ -18,13 +18,13 @@ class TransactionManager(
     fun edit(updatedTransaction: Transaction): List<String> {
         val errorList = updatedTransaction.validateTransaction()
 
-        if(!errorList.isNotEmpty()){
+        if (!errorList.isNotEmpty()) {
             storage.edit(updatedTransaction)
         }
         return errorList
     }
 
-    fun delete(transactionId: String) : Boolean{
+    fun delete(transactionId: String): Boolean {
         return storage.delete(transactionId)
     }
 
@@ -34,22 +34,6 @@ class TransactionManager(
 
     fun getMonthlySummary(): List<MonthlySummary> {
         val transactions = getAll()
-
-        val grouped = transactions.groupBy {
-            it.date.format(DateTimeFormatter.ofPattern("MM-yyyy"))
-        } // result is map<LocalDate, List<Transaction>>
-
-        val summaries = grouped.map { (monthYear, transactions) ->
-            val totalIncome = transactions
-                .filter { it.type == TransactionType.INCOME }
-                .sumOf { it.amount?: 0.0 }
-
-            val totalExpense = transactions
-                .filter { it.type == TransactionType.EXPENSE }
-                .sumOf { it.amount?: 0.0 }
-
-            MonthlySummary(monthYear, totalIncome, totalExpense)
-        }
-        return summaries.sortedBy { it.monthYear }
+        return summaryGenerator.generateMonthlySummaries(transactions)
     }
 }
